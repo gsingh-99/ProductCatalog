96d896353f9c0f0203308d6c50809e7743e59074
Gurparkash Singh <gurparkashsingh99@gmail.com>
2022-03-30 21:09:00 +0200

    changes

--- src/main/java/at/technikum/wien/singh/ProductCatalog/Repository/ProductTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package at.technikum.wien.singh.ProductCatalog.Repository;
-
-import at.technikum.wien.singh.ProductCatalog.Model.ProductType;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductTypeRepository extends CrudRepository<ProductType, Integer> {
-}
